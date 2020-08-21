d43e95f32b2980420911345ea330f5aa7feaf54e
Rick Brown <ricky.brown.00@gmail.com>
2020-08-21 02:20:36 +0100

    adds prettier

--- webpack/base.js
+++ webpack/base.js
@@ -1,7 +1,7 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -56,4 +56,4 @@ module.exports = {
       template: './index.html',
     }),
   ],
-};
+}
